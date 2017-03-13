83d804522e8e0e0bb1f917c876d62fc2d030a72b
dsmhishi <mhishidestinys@gmail.com>
2017-03-13 10:34:17 -0500

    Handling multiple file uploads - in progress

--- app/src/main/java/com/firebase/ui/AddNewItem.java
+++ app/src/main/java/com/firebase/ui/AddNewItem.java
@@ -1,21 +1,41 @@
 package com.firebase.ui;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.bisonswap.bisonswap.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.StorageReference;
+
+import java.net.URI;
 
 public class AddNewItem extends AppCompatActivity {
 
+    private StorageReference storageReference;
+    private DatabaseReference databaseReference;
     private String itemName;
     private String itemCategory;
     private String itemRating;
     private String itemDescription;
+
+    private Uri[] imgUriArray = new Uri[5];
+    private int IMAGE_INDEX;
+
+    private static final String STORAGE_PATH = "image/";
+    private static final String DATABASE_PATH = "image";
+    private static final int REQUEST_CODE = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,8 +79,34 @@ public class AddNewItem extends AppCompatActivity {
         });
     }
 
-    private void sendData() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imgUriArray[IMAGE_INDEX] = data.getData();
+        }
+    }
+
+    private String getImageExt(Uri uri) {
+        ContentResolver resolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(resolver.getType(uri));
+    }
+
+    public void sendData(View v) {
+        ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setTitle("Submitting...");
+        dialog.show();
+        //remember to handle all images chosen
+        StorageReference sRef = storageReference.child(STORAGE_PATH + System.currentTimeMillis()+getImageExt(imgUriArray[0]));
         itemName = ((EditText) findViewById(R.id.itemName)).getText().toString();
         itemDescription = ((EditText) findViewById(R.id.itemDescription)).getText().toString();
+    }
+
+    public void browseImage(View v) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Image"), REQUEST_CODE);
     }
 }
